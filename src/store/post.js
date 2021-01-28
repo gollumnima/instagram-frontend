b89854751565f10f2daf0d91206b0fce54876497
Doori Kim <doori.alice.kim@gmail.com>
2021-01-28 22:14:26 +0900

    ADD : lodash memoization

--- src/store/post.js
+++ src/store/post.js
@@ -1,5 +1,6 @@
 import { createSlice } from "@reduxjs/toolkit";
 import { instaAPI } from "utils/axios.wrapper";
+import _ from "lodash";
 
 const postSlice = createSlice({
   name: "post",
@@ -21,49 +22,54 @@ export default postSlice.reducer;
 
 const { setPost, setPostList } = postSlice.actions;
 
-export const getPosts = () => async dispatch => {
-  try {
-    const { data } = await instaAPI.get(`/api/posts`);
-    dispatch(setPostList(data.rows));
-  } catch (err) {
-    console.error(err);
-  }
-};
+export const getPosts = () =>
+  _.memoize(async dispatch => {
+    try {
+      const { data } = await instaAPI.get(`/api/posts`);
+      dispatch(setPostList(data.rows));
+    } catch (err) {
+      console.error(err);
+    }
+  });
 
-export const getPost = postId => async dispatch => {
-  try {
-    const { data } = await instaAPI.get(`/api/posts/${postId}`);
+export const getPost = postId =>
+  _.memoize(async dispatch => {
+    try {
+      const { data } = await instaAPI.get(`/api/posts/${postId}`);
 
-    // getPostNumber(data.id);
-    // setImgURL(data.images[0].url); ???
-    // setUserID(data.User.username); ???
-    // setContent(data.content); ???
+      // getPostNumber(data.id);
+      // setImgURL(data.images[0].url); ???
+      // setUserID(data.User.username); ???
+      // setContent(data.content); ???
 
-    dispatch(setPost(data));
-  } catch (err) {
-    console.error(err);
-  }
-};
+      dispatch(setPost(data));
+    } catch (err) {
+      console.error(err);
+    }
+  });
 
-export const deletePost = postId => async dispatch => {
-  await instaAPI.delete(`/api/posts/${postId}`);
-  await dispatch(getPosts());
-  //const { data } = instaAPI.delete(`/api/posts/${postId}`);
-  //await dispatch(getPost(data));
-};
+export const deletePost = postId =>
+  _.memoize(async dispatch => {
+    await instaAPI.delete(`/api/posts/${postId}`);
+    await dispatch(getPosts());
+    //const { data } = instaAPI.delete(`/api/posts/${postId}`);
+    //await dispatch(getPost(data));
+  });
 
 // export const getAllPost = postList => async dispatch => {
 //   await dispatch(setAllPost(postList));
 // };
 
-export const likePost = postID => async dispatch => {
-  await instaAPI.post(`/api/posts/${postID}/like`);
-  await dispatch(getPost(postID));
-  await dispatch(getPosts());
-};
+export const likePost = postID =>
+  _.memoize(async dispatch => {
+    await instaAPI.post(`/api/posts/${postID}/like`);
+    await dispatch(getPost(postID));
+    await dispatch(getPosts());
+  });
 
-export const unlikePost = postID => async dispatch => {
-  await instaAPI.delete(`/api/posts/${postID}/like`);
-  await dispatch(getPost(postID));
-  await dispatch(getPosts());
-};
+export const unlikePost = postID =>
+  _.memoize(async dispatch => {
+    await instaAPI.delete(`/api/posts/${postID}/like`);
+    await dispatch(getPost(postID));
+    await dispatch(getPosts());
+  });
--- src/store/user.js
+++ src/store/user.js
@@ -1,6 +1,7 @@
 import { createSlice } from "@reduxjs/toolkit";
 import { instaAPI } from "utils/axios.wrapper";
 import { authToken } from "utils/localStorage.wrapper";
+import _ from "lodash";
 
 const userSlice = createSlice({
   name: "user",
@@ -44,33 +45,35 @@ const {
   setFollowings
 } = userSlice.actions;
 
-export const signUp = (username, name, password) => async dispatch => {
-  try {
-    const { data } = await instaAPI.post(`/api/users`, {
-      username,
-      name,
-      password
-    });
-  } catch (err) {
-    console.error(err);
-  }
-};
+export const signUp = (username, name, password) =>
+  _.memoize(async dispatch => {
+    try {
+      const { data } = await instaAPI.post(`/api/users`, {
+        username,
+        name,
+        password
+      });
+    } catch (err) {
+      console.error(err);
+    }
+  });
 
-export const login = (username, password, callback) => async dispatch => {
-  try {
-    const { data } = await instaAPI.post(`/api/users/login`, {
-      username,
-      password
-    });
-    const { user, token } = data;
-    dispatch(setUserInfo(user));
-    authToken.set(token);
-    if (callback) return callback(user);
-  } catch (err) {
-    dispatch(setUserInfo(null));
-    authToken.remove();
-  }
-};
+export const login = (username, password, callback) =>
+  _.memoize(async dispatch => {
+    try {
+      const { data } = await instaAPI.post(`/api/users/login`, {
+        username,
+        password
+      });
+      const { user, token } = data;
+      dispatch(setUserInfo(user));
+      authToken.set(token);
+      if (callback) return callback(user);
+    } catch (err) {
+      dispatch(setUserInfo(null));
+      authToken.remove();
+    }
+  });
 
 export const getSelf = () => dispatch => {
   instaAPI.get(`/api/users/self`).then(({ data }) => {
@@ -88,67 +91,72 @@ export const deleteProfile = () => () => {
   instaAPI.delete(`/api/users/self/persona`);
 };
 
-export const findUser = username => async dispatch => {
-  const { data } = await instaAPI.get(`/api/users/username/${username}`);
-  dispatch(setFoundUser(data));
-};
+export const findUser = username =>
+  _.memoize(async dispatch => {
+    const { data } = await instaAPI.get(`/api/users/username/${username}`);
+    dispatch(setFoundUser(data));
+  });
 
 // 해당 id를 가진 유저 follow
-export const follow = id => async dispatch => {
-  try {
-    await dispatch(
-      setFollowers({
-        rows: [],
-        count: 0
-      })
-    );
-    const { data } = await instaAPI.post(`/api/users/${id}/followers`);
-    const { rows, count } = data;
-    await dispatch(setFollowers({ rows, count }));
-    return data;
-  } catch (err) {
-    console.error(err);
-  }
-};
+export const follow = id =>
+  _.memoize(async dispatch => {
+    try {
+      await dispatch(
+        setFollowers({
+          rows: [],
+          count: 0
+        })
+      );
+      const { data } = await instaAPI.post(`/api/users/${id}/followers`);
+      const { rows, count } = data;
+      await dispatch(setFollowers({ rows, count }));
+      return data;
+    } catch (err) {
+      console.error(err);
+    }
+  });
 
 // 해당 id를 가진 유저 unfollow
-export const unfollow = id => async dispatch => {
-  instaAPI.delete(`/api/users/${id}/followers`);
-  dispatch(setFollowers(null));
-};
+export const unfollow = id =>
+  _.memoize(async dispatch => {
+    instaAPI.delete(`/api/users/${id}/followers`);
+    dispatch(setFollowers(null));
+  });
 
 // 해당 id를 가진 유저의 follower 목록
-export const getFollowers = id => async dispatch => {
-  try {
-    await dispatch(
-      setFollowers({
-        rows: [],
-        count: 0
-      })
-    );
-    const { data } = await instaAPI.get(`/api/users/${id}/followers`);
-    const { rows, count } = data;
-    await dispatch(setFollowers({ rows, count }));
-    return data;
-  } catch (err) {
-    console.error(err);
-  }
-};
+export const getFollowers = id =>
+  _.memoize(async dispatch => {
+    try {
+      await dispatch(
+        setFollowers({
+          rows: [],
+          count: 0
+        })
+      );
+      const { data } = await instaAPI.get(`/api/users/${id}/followers`);
+      const { rows, count } = data;
+      await dispatch(setFollowers({ rows, count }));
+      return data;
+    } catch (err) {
+      console.error(err);
+    }
+  });
 
 // 해당 id를 가진 유저의 following 목록
-export const getFollowings = id => async dispatch => {
-  try {
-    await dispatch(
-      setFollowers({
-        rows: [],
-        count: 0
-      })
-    );
-    const { data } = await instaAPI.get(`/api/users/${id}/followings`);
-    const { rows, count } = data;
-    await dispatch(setFollowings({ rows, count }));
-    return data;
-  } catch (err) {
-    console.error(err);
-  }
-};
+export const getFollowings = id =>
+  _.memoize(async dispatch => {
+    try {
+      await dispatch(
+        setFollowers({
+          rows: [],
+          count: 0
+        })
+      );
+      const { data } = await instaAPI.get(`/api/users/${id}/followings`);
+      const { rows, count } = data;
+      await dispatch(setFollowings({ rows, count }));
+      return data;
+    } catch (err) {
+      console.error(err);
+    }
+  });
